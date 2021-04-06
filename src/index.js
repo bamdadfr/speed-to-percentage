3bec411c05a7e23ba61a9efff9b529297a29c8a6
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-04-06 22:24:12 +0200

    fix(jsdoc): added `function` and `description` tags [force-release]

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 /**
- * speedToPercentage()
- * calculate percentage from playback speed
- *
- * normal playback speed is 1
- *
+ * @function speedToPercentage
+ * @description calculate percentage from playback speed, normal playback speed is 1
  * @param {number} speed - playback speed
  * @param {number} digits - number of digits to appear after the decimal point
  *      value between 0 and 20
@@ -13,13 +10,13 @@
 
 const speedToPercentage = (speed, digits = 0) => {
 
-    if (typeof speed !== 'number') throw new TypeError ('speed parameter should be a number')
+    if (typeof speed !== 'number') throw new TypeError('speed parameter should be a number')
 
-    if (typeof digits !== 'number') throw new TypeError ('digits parameter should be a number')
+    if (typeof digits !== 'number') throw new TypeError('digits parameter should be a number')
 
-    if (digits < 0 || digits > 20) throw new TypeError ('digits parameter should be between 0 and 20')
+    if (digits < 0 || digits > 20) throw new TypeError('digits parameter should be between 0 and 20')
 
-    return (speed * 100 - 100).toFixed (digits)
+    return (speed * 100 - 100).toFixed(digits)
 
 }
 
